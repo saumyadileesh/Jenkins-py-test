73c60cbf950f16d0b88542dffdca7c41aef94d97
saumyadileesh <72030306+saumyadileesh@users.noreply.github.com>
2025-10-07 04:30:41 +0530

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -2,7 +2,7 @@ import pytest
 from add import add
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 4) == 6
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(1.5, 2.5) == 4.0
